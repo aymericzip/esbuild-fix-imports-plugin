3f2e77034e2d8097d6391aa9ddc87cab4101591b
Aymeric PINEAU <ay.pineau@gmail.com>
2024-10-24 02:02:44 -0600

    feat: publish plugin

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { type Plugin } from "esbuild";
+import { fixAliasPlugin } from "./fixAliasPlugin";
+import { fixExtensionsPlugin } from "./fixExtensionsPlugin";
+import { fixFolderImportsPlugin } from "./fixFolderImportsPlugin";
+
+/**
+ * Combined plugin that runs fixAliasPlugin, fixFolderImportsPlugin, and fixExtensionsPlugin.
+ *
+ * @returns {Plugin} An ESBuild plugin.
+ */
+export const fixImportsPlugin = (): Plugin => ({
+  name: "fixImportsPlugin",
+  setup(build) {
+    // Apply each plugin's setup function
+    fixAliasPlugin().setup(build);
+    fixFolderImportsPlugin().setup(build);
+    fixExtensionsPlugin().setup(build);
+  },
+});
+
+// Optionally export individual plugins
+export { fixAliasPlugin, fixExtensionsPlugin, fixFolderImportsPlugin };
---
+++ tsup.config.ts
@@ -0,0 +1,35 @@
+import { type Options, defineConfig } from "tsup";
+import { fixImportsPlugin } from "./src";
+
+export const commonOptions: Options = {
+  entry: ["src/**/*"],
+  target: "esnext",
+  dts: true,
+  external: ["fs", "path"],
+  clean: true,
+  sourcemap: true,
+  bundle: false,
+  tsconfig: "./tsconfig.json",
+  esbuildPlugins: [fixImportsPlugin()],
+};
+
+export const option: Options[] = [
+  {
+    ...commonOptions,
+    format: ["cjs"],
+    outDir: "dist/cjs",
+    outExtension: () => ({
+      js: ".cjs",
+    }),
+  },
+  {
+    ...commonOptions,
+    format: ["esm"],
+    outDir: "dist/esm",
+    outExtension: () => ({
+      js: ".mjs",
+    }),
+  },
+];
+
+export default defineConfig(option);
